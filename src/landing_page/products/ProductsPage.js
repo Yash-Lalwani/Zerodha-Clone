cbb4b5df571cdb9d75514a7342ad66563a92726d
Yash Lalwani <yash@Yashs-MacBook-Air.local>
2025-06-13 19:27:39 -0400

    Basic Setup of all the pages

---
+++ src/landing_page/products/ProductsPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RigthSection";
+import Universe from "./Universe";
+
+import Footer from "../Footer";
+import Navbar from "../Navbar";
+import OpenAccount from "../OpenAccount";
+
+function ProductsPage() {
+  return (
+    <>
+      <Hero />
+      <LeftSection />
+      <RightSection />
+      <Universe />
+      <Footer />
+      <Navbar />
+      <OpenAccount />
+    </>
+  );
+}
+
+export default ProductsPage;
---
+++ src/landing_page/support/SupportPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import CreateTicket from "./CreateTicket";
+import Hero from "./Hero";
+
+import Footer from "../Footer";
+import Navbar from "../Navbar";
+
+function SupportPage() {
+  return (
+    <>
+      <CreateTicket />
+      <Hero />
+      <Footer />
+      <Navbar />
+    </>
+  );
+}
+
+export default SupportPage;
