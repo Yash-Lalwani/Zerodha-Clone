a3efb1bd47615b7abcf9c1605c4a1d2d7f2d5eb9
Yash Lalwani <yash@Yashs-MacBook-Air.local>
2025-06-13 20:01:44 -0400

    Added Assets and Setup BootStrap and FontAwesome

--- src/index.js
+++ src/index.js
@@ -12,88 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HomePage />
-    <p>
-      Lorem ipsum dolor sit amet. Est iste corrupti ut porro rerum est
-      consequatur mollitia 33 voluptates deleniti eos accusantium quia! Et
-      officia tempora sed veniam dignissimos ut dicta voluptas in error
-      voluptatem ab amet eius! Et minima maxime non corrupti harum qui enim
-      dicta et aliquam eveniet eum neque internos a obcaecati ratione cum odio
-      molestias! Sed deserunt explicabo in dolores neque id pariatur excepturi?
-      Hic quae enim a incidunt cupiditate aut voluptatibus molestiae aut ipsam
-      assumenda id ipsa inventore eum omnis molestiae quo provident
-      voluptatibus. Non consequatur accusamus qui vitae blanditiis eum dolores
-      suscipit! Ea asperiores soluta est praesentium dolorem ut totam enim. Rem
-      aperiam explicabo sit nostrum quos aut laboriosam autem.
-    </p>
     <AboutPage />
-    <p>
-      Lorem ipsum dolor sit amet. Est iste corrupti ut porro rerum est
-      consequatur mollitia 33 voluptates deleniti eos accusantium quia! Et
-      officia tempora sed veniam dignissimos ut dicta voluptas in error
-      voluptatem ab amet eius! Et minima maxime non corrupti harum qui enim
-      dicta et aliquam eveniet eum neque internos a obcaecati ratione cum odio
-      molestias! Sed deserunt explicabo in dolores neque id pariatur excepturi?
-      Hic quae enim a incidunt cupiditate aut voluptatibus molestiae aut ipsam
-      assumenda id ipsa inventore eum omnis molestiae quo provident
-      voluptatibus. Non consequatur accusamus qui vitae blanditiis eum dolores
-      suscipit! Ea asperiores soluta est praesentium dolorem ut totam enim. Rem
-      aperiam explicabo sit nostrum quos aut laboriosam autem.
-    </p>
     <PricingPage />
-    <p>
-      Lorem ipsum dolor sit amet. Est iste corrupti ut porro rerum est
-      consequatur mollitia 33 voluptates deleniti eos accusantium quia! Et
-      officia tempora sed veniam dignissimos ut dicta voluptas in error
-      voluptatem ab amet eius! Et minima maxime non corrupti harum qui enim
-      dicta et aliquam eveniet eum neque internos a obcaecati ratione cum odio
-      molestias! Sed deserunt explicabo in dolores neque id pariatur excepturi?
-      Hic quae enim a incidunt cupiditate aut voluptatibus molestiae aut ipsam
-      assumenda id ipsa inventore eum omnis molestiae quo provident
-      voluptatibus. Non consequatur accusamus qui vitae blanditiis eum dolores
-      suscipit! Ea asperiores soluta est praesentium dolorem ut totam enim. Rem
-      aperiam explicabo sit nostrum quos aut laboriosam autem.
-    </p>
     <ProductsPage />
-    <p>
-      Lorem ipsum dolor sit amet. Est iste corrupti ut porro rerum est
-      consequatur mollitia 33 voluptates deleniti eos accusantium quia! Et
-      officia tempora sed veniam dignissimos ut dicta voluptas in error
-      voluptatem ab amet eius! Et minima maxime non corrupti harum qui enim
-      dicta et aliquam eveniet eum neque internos a obcaecati ratione cum odio
-      molestias! Sed deserunt explicabo in dolores neque id pariatur excepturi?
-      Hic quae enim a incidunt cupiditate aut voluptatibus molestiae aut ipsam
-      assumenda id ipsa inventore eum omnis molestiae quo provident
-      voluptatibus. Non consequatur accusamus qui vitae blanditiis eum dolores
-      suscipit! Ea asperiores soluta est praesentium dolorem ut totam enim. Rem
-      aperiam explicabo sit nostrum quos aut laboriosam autem.
-    </p>
     <Signup />
-    <p>
-      Lorem ipsum dolor sit amet. Est iste corrupti ut porro rerum est
-      consequatur mollitia 33 voluptates deleniti eos accusantium quia! Et
-      officia tempora sed veniam dignissimos ut dicta voluptas in error
-      voluptatem ab amet eius! Et minima maxime non corrupti harum qui enim
-      dicta et aliquam eveniet eum neque internos a obcaecati ratione cum odio
-      molestias! Sed deserunt explicabo in dolores neque id pariatur excepturi?
-      Hic quae enim a incidunt cupiditate aut voluptatibus molestiae aut ipsam
-      assumenda id ipsa inventore eum omnis molestiae quo provident
-      voluptatibus. Non consequatur accusamus qui vitae blanditiis eum dolores
-      suscipit! Ea asperiores soluta est praesentium dolorem ut totam enim. Rem
-      aperiam explicabo sit nostrum quos aut laboriosam autem.
-    </p>
     <SupportPage />
-    <p>
-      Lorem ipsum dolor sit amet. Est iste corrupti ut porro rerum est
-      consequatur mollitia 33 voluptates deleniti eos accusantium quia! Et
-      officia tempora sed veniam dignissimos ut dicta voluptas in error
-      voluptatem ab amet eius! Et minima maxime non corrupti harum qui enim
-      dicta et aliquam eveniet eum neque internos a obcaecati ratione cum odio
-      molestias! Sed deserunt explicabo in dolores neque id pariatur excepturi?
-      Hic quae enim a incidunt cupiditate aut voluptatibus molestiae aut ipsam
-      assumenda id ipsa inventore eum omnis molestiae quo provident
-      voluptatibus. Non consequatur accusamus qui vitae blanditiis eum dolores
-      suscipit! Ea asperiores soluta est praesentium dolorem ut totam enim. Rem
-      aperiam explicabo sit nostrum quos aut laboriosam autem.
-    </p>
   </React.StrictMode>
 );
--- src/landing_page/home/Awards.js
+++ src/landing_page/home/Awards.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Awards() {
-  return <h1>Awards</h1>;
+  return (
+    <h1>
+      Awards <i class="fa-regular fa-circle-user"></i>
+    </h1>
+  );
 }
 
 export default Awards;
