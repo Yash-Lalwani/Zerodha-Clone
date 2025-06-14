b1750583adf90a4626a5dc45fe163fc0da5e021a
Yash Lalwani <yash@Yashs-MacBook-Air.local>
2025-06-13 20:31:38 -0400

    resolved merge conflicts

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import HomePage from "./landing_page/home/HomePage";
-import AboutPage from "./landing_page/about/AboutPage";
-import PricingPage from "./landing_page/pricing/PricingPage";
-import ProductsPage from "./landing_page/products/ProductsPage";
-import Signup from "./landing_page/signup/Signup";
-import SupportPage from "./landing_page/support/SupportPage";
+// import AboutPage from "./landing_page/about/AboutPage";
+// import PricingPage from "./landing_page/pricing/PricingPage";
+// import ProductsPage from "./landing_page/products/ProductsPage";
+// import Signup from "./landing_page/signup/Signup";
+// import SupportPage from "./landing_page/support/SupportPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HomePage />
-    <AboutPage />
+    {/* <AboutPage />
     <PricingPage />
     <ProductsPage />
     <Signup />
-    <SupportPage />
+    <SupportPage /> */}
   </React.StrictMode>
 );
