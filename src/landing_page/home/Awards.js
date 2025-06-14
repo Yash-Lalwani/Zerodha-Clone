73e242a898a9bae7e1c1b18c767fb9106874b4e5
Yash Lalwani <yash@Yashs-MacBook-Air.local>
2025-06-13 20:11:08 -0400

    save

--- src/landing_page/home/Awards.js
+++ src/landing_page/home/Awards.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Awards() {
-  return (
-    <h1>
-      Awards <i class="fa-regular fa-circle-user"></i>
-    </h1>
-  );
+  return <h1>Awards</h1>;
 }
 
 export default Awards;
