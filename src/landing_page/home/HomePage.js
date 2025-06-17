d0f5a55f1c3e0f783329889ad3a4e63c4ff70791
Yash Lalwani <yash@Yashs-MacBook-Air.local>
2025-06-17 19:17:39 -0400

    Routing Setup for each component's path

--- src/landing_page/home/HomePage.js
+++ src/landing_page/home/HomePage.js
@@ -6,20 +6,16 @@ import Stats from "./Stats";
 import Pricing from "./Pricing";
 
 import OpenAccount from "../OpenAccount";
-import Footer from "../Footer";
-import Navbar from "../Navbar";
 
 function HomePage() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Awards />
       <Stats />
       <Pricing />
       <Education />
       <OpenAccount />
-      <Footer />
     </>
   );
 }
--- src/landing_page/pricing/PricingPage.js
+++ src/landing_page/pricing/PricingPage.js
@@ -2,16 +2,11 @@ import React from "react";
 import Brokerage from "./Brokerage";
 import Hero from "./Hero";
 
-import Footer from "../Footer";
-import Navbar from "../Navbar";
-
 function PricingPage() {
   return (
     <>
       <Brokerage />
       <Hero />
-      <Footer />
-      <Navbar />
     </>
   );
 }
