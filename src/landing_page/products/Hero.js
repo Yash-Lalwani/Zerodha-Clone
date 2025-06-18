02c14fa421ab3a4e5be835a46c2a66ffeacb5b1f
Yash Lalwani <yash@macbookair.mynetworksettings.com>
2025-06-18 00:13:52 -0400

    product component setup

--- src/landing_page/products/Hero.js
+++ src/landing_page/products/Hero.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <div className="container mt-5 p-2 border-bottom">
+      <div className="row text-center mt-5 mb-5 ">
+        <h1 style={{ fontSize: "3rem", lineHeight: "1.2" }}>
+          Zerodha Products
+        </h1>
+        <p className="p-2" style={{ fontSize: "1.3rem" }}>
+          Sleek, modern, and intuitive trading platforms
+        </p>
+        <p>
+          Check out our{" "}
+          <a href="" style={{ textDecoration: "none" }}>
+            investment offerings <i class="fa-solid fa-arrow-right"></i>
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
