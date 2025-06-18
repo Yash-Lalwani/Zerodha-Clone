6910860d81fce42ffde05d0447f801cbb3eea5c8
Yash Lalwani <yash@macbookair.mynetworksettings.com>
2025-06-18 04:57:56 -0400

    support page setup

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -1,7 +1,138 @@
 import React from "react";
 
 function CreateTicket() {
-  return <h1>CreateTicket</h1>;
+  return (
+    <div className="container">
+      <div className="row">
+        <p className="text-muted fs-5 mt-5">
+          To create a ticket, select a relevant topic
+        </p>
+
+        {/* Account Opening */}
+        <div className="col-4 fs-5 mb-5 mt-5">
+          <p className="ms-2">
+            <i className="fa-solid fa-circle-plus me-2"></i> Account Opening
+          </p>
+          <a href="#" style={{ textDecoration: "none", lineHeight: "1.2rem" }}>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Resident individual
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>Minor</p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Non Resident Indian (NRI)
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Company, Partnership, HUF and LLP
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>Glossary</p>
+          </a>
+        </div>
+
+        {/* Your Zerodha Account */}
+        <div className="col-4 fs-5 mb-5 mt-5">
+          <p className="ms-2">
+            <i className="fa-regular fa-user me-3"></i>Your Zerodha Account
+          </p>
+          <a href="#" style={{ textDecoration: "none", lineHeight: "1.2rem" }}>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Your Profile
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Account modification
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Client Master Report (CMR) and Depository Participant (DP)
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>Nomination</p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Transfer and conversion of securities
+            </p>
+          </a>
+        </div>
+
+        {/* Kite */}
+        <div className="col-4 fs-5 mb-5 mt-5">
+          <p className="ms-2">
+            <i className="fa-solid fa-chart-simple me-3"></i>Kite
+          </p>
+          <a href="#" style={{ textDecoration: "none", lineHeight: "1.2rem" }}>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>IPO</p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Trading FAQs
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Margin Trading Facility (MTF) and Margins
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Charts and orders
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Alerts and Nudges
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>General</p>
+          </a>
+        </div>
+
+        {/* Funds */}
+        <div className="col-4 fs-5 mb-5 mt-5">
+          <p className="ms-2">
+            <i className="fa-solid fa-wallet me-2"></i> Funds
+          </p>
+          <a href="#" style={{ textDecoration: "none", lineHeight: "1.2rem" }}>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>Add money</p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Withdraw money
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Add bank accounts
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>eMandates</p>
+          </a>
+        </div>
+
+        {/* Console */}
+        <div className="col-4 fs-5 mb-5 mt-5">
+          <p className="ms-2">
+            <i className="fa-brands fa-chrome me-3"></i> Console
+          </p>
+          <a href="#" style={{ textDecoration: "none", lineHeight: "1.2rem" }}>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>Portfolio</p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Corporate actions
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Funds statement
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>Reports</p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>Profile</p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>Segments</p>
+          </a>
+        </div>
+
+        {/* Coin */}
+        <div className="col-4 fs-5 mb-5 mt-5">
+          <p className="ms-2">
+            <i className="fa-solid fa-coins me-2"></i> Coin
+          </p>
+          <a href="#" style={{ textDecoration: "none", lineHeight: "1.2rem" }}>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Mutual funds
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              National Pension Scheme (NPS)
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Features on Coin
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>
+              Payments and Orders
+            </p>
+            <p style={{ fontSize: "0.9rem", marginLeft: "45px" }}>General</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -1,7 +1,55 @@
 import React from "react";
 
 function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <section className="container-fluid" id="supportHero">
+      <div className="p-5 pb-1" id="supportWrapper">
+        <h4>Support Portal</h4>
+        <a href="">Track Tickets</a>
+      </div>
+
+      <div className="row p-3 m-3">
+        {/* Left Column */}
+        <div className="col-6 p-3 pt-1">
+          <h1 className="fs-3 mb-5">
+            Search for an answer or browse help topics to create a ticket
+          </h1>
+
+          <input
+            className="mb-4" // Adds space below the input box
+            placeholder="Eg. how do I activate F&O"
+          />
+          <br />
+          <a href="" className="me-3">
+            Track account opening
+          </a>
+          <a href="" className="me-3 ">
+            Track segment activation
+          </a>
+          <a href="">Intraday margins</a>
+          <br />
+          <a href="" style={{ lineHeight: "3rem" }}>
+            Kite user manual
+          </a>
+        </div>
+
+        {/* Right Column */}
+        <div className="col-6 p-5">
+          <h1 className="fs-3" style={{ marginLeft: "90px" }}>
+            Featured
+          </h1>
+          <ol>
+            <li style={{ marginLeft: "100px", lineHeight: "4rem" }}>
+              <a href="">Current Takeovers and Delisting – January 2024</a>
+            </li>
+            <li style={{ marginLeft: "100px" }}>
+              <a href="">Latest Intraday leverages – MIS & CO</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
